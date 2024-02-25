e7559aebaa3c591047d002318d42613dbff2b706
Srvictorchinea <sr.victorchinea@gmail.com>
2024-02-25 20:27:14 +0000

    [ADD] riverpod and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,27 @@
-import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
 
+import 'package:favorite_places/screens/places.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(255, 102, 6, 247),
   background: const Color.fromARGB(255, 56, 49, 66),
-  );
-
-final Theme = ThemeData().copyWith(
-  scaffoldBackgroundColor: colorScheme.background,
-  colorScheme: colorScheme,
-  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
-    titleSmall: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold
-    ),
-    titleMedium: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold
-    ),
-    titleLarge: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold
-    ),
-  )
 );
+
+final theme = ThemeData().copyWith(
+    scaffoldBackgroundColor: colorScheme.background,
+    colorScheme: colorScheme,
+    textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+      titleSmall: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+      titleMedium: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+      titleLarge: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+    ));
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(child: MyApp()),
   );
 }
 
@@ -39,7 +33,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Great Places',
-      theme: Theme,
+      theme: theme,
       home: const PlacesScreen(),
     );
   }
